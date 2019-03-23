e1e0cdd873c108da3032e20b1b9c26963690f4d9
Nik B <nikolaus.bertino@gmail.com>
2019-03-23 11:50:55 -0400

    changing links in head

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
    <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet"href="/custom.css">
-        <script type="text/javascript" src="/note.js"></script>
+        <link rel="stylesheet"href="/static/css/custom.css">
+        <script type="text/javascript" src="/static/js/note.js"></script>
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
    </head>
    <body>

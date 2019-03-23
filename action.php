a690637c9891c478ee0461b899bade4b079754a9
Nik B <nikolaus.bertino@gmail.com>
2019-03-22 20:28:56 -0400

    adding constants

--- action.php
+++ action.php
@@ -1,25 +1,21 @@
 <?php
-
-
-
-
-
+$noteDir = "./notes/";
+$noteExtension = ".txt";
 if(isset($_GET['filename']) && $_GET['filename'] !== ''){
-
     if(isset($_GET['cmd']) && $_GET['cmd'] === 'getData'){
-
-        $data = file_get_contents($_GET['filename']);
+        $file = $noteDir.$_GET['filename'].$noteExtension;
+        $data = file_get_contents($file);
         echo json_encode(array('success' => $data));
-	die;
+	    die;
     }
-    
     $name = $_GET['filename'];
     $text =  json_decode($_POST['data'],true)['text'];
-    $fileCreated = file_put_contents($name, $text);
+    $fileCreated = file_put_contents($noteDir.$name.$noteExtension, $text);
     echo json_encode(array('success' => $fileCreated ? $name : $fileCreated));
 }else{
     $text =  json_decode($_POST['data'],true)['text'];
-    $name = './notes/'.time().'.txt'; 
+    $time = time();
+    $name = './notes/'.$time.'.txt'; 
     $fileCreated = file_put_contents($name, $text);
-    echo json_encode(array('success' => $fileCreated ? $name : $fileCreated)); 
+    echo json_encode(array('success' => $fileCreated ? $time : $fileCreated)); 
 }

85aa294c1b7796e664e516dadc69e6ede677460b
Nik B <nikolaus.bertino@gmail.com>
2019-03-22 20:31:03 -0400

    adding new html and styles

--- index.php
+++ index.php
@@ -1,39 +1,18 @@
 <html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
-<link rel="stylesheet"href="/custom.css">
-<script type="text/javascript" src="/functions.js"></script>
-
-</head>
-<body>
-<div class="container">
-
-        <div class="mainapp">
-            <div class="clean">
-                <div class="controls">
-                    <i class="fa fa-arrow-left" aria-hidden="true"></i>
-                    <span class="right">
-                <i class="fa fa-reply" aria-hidden="true"></i>
-                <i class="fa fa-share-alt" aria-hidden="true"></i>
-               <i class="fa fa-ellipsis-v"aria-hidden="true"></i>
-            </span>
-                </div>
-                <div class="notes">
-                    <h1>Notes</h1> <i class="fa fa-bookmark" aria-hidden="true"></i>
-                    <span class="date">{time}</span>
-                    <textarea></textarea>
-	            <input id="filename" type="hidden">
-	</div>
-        <span class="extra">
-            <img src="/pencil.png">
-            <img src="/save.png"class="active">
-            <img src="/bin.png">
-   	    <a href="/" target="_blank"><img src="/plus.png"></a>
-               </span>
-            </div>
-                        <div class=" btm "></div>
-        </div>
-    </div>
-</body>
+   <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet"href="/custom_02.css">
+        <script type="text/javascript" src="/functions_02.js"></script>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+   </head>
+   <body>
+      <div class="note">
+      <span class="date">Friday 22 March, 2019 at 7:11PM</span>
+      <span class="icons">
+         <i class="fa fa-pencil fa-lg" ></i>
+         <i class="fa fa-trash-o fa-lg" ></i>
+      </span>
+      <div class="markdown-body textarea" contenteditable="true"></div>
+      <input id="filename" type="hidden">
+   </body>
 </html>

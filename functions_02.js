c84838438629f7b84d48f28b73345e33eab6c21e
Nik B <nikolaus.bertino@gmail.com>
2019-03-22 20:32:21 -0400

    adding new js file

---
+++ functions_02.js
@@ -0,0 +1,75 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function() {
+    const getDate = function getDate () {
+        const today = new Date();
+        document.getElementsByClassName('date')[0].innerText =  [today.getMonth(),today.getDate(),today.getFullYear()].join("/") + " " + today.toString().split(' ').slice(4,6).join(" ");
+    }
+    setInterval(getDate, 1000*20);
+    getDate();
+
+    let params = new URLSearchParams(window.location.search);
+	if (params.has('note')) {
+		document.getElementById('filename').value = params.get('note');
+		getTextData(document.getElementById('filename').value);
+		function getTextData(filename) {
+			fetch('/action.php?cmd=getData&filename=' + filename,{
+				method:'GET',
+				headers: {'Content-type':'application/x-www-form-urlencoded'}
+			}).then((...t)=>{
+				t[0].json().then((a)=>{
+					if(a.success){
+						let data = a.success;
+						document.getElementsByClassName('textarea')[0].innerText = atob(data);
+					}
+				});
+			});
+		}
+    }
+    
+    
+})
+const save = () => {
+    const url = '/action.php?cmd=save&filename='+document.getElementById('filename').value;
+    fetch(url,{
+        method:'POST',
+        headers: {'Content-type':'application/x-www-form-urlencoded'},
+        body: `data=${JSON.stringify({text:btoa(document.getElementsByClassName('textarea')[0].innerText)})}`
+    }).then((...t)=>{
+        console.log(t);
+        t[0].json().then((a)=>{
+            if(a.success){
+                document.getElementById('filename').value = a.success;
+            }
+        });
+    });
+}
+
+const keyHandler = (() => {
+    let lastKeyPress = []
+    //when key is pressed...
+    document.onkeydown = function (e) {
+
+        e = e || window.event;
+
+        switch (e.keyCode) {
+            case 13:
+                //'enter' is pressed
+                //todo code
+                break;
+            case 67:
+                //'c' is pressed
+                if (lastKeyPress[lastKeyPress.length - 1] == 'Control') {
+                    setTimeout(function () {//todo code when alt + ctrl + c is pressed
+                        console.log('saving');
+                        save();
+                        lastKeyPress = [];
+                    }, 500);
+                }
+        }
+
+        //push key name into array
+        lastKeyPress.push(e.key)
+
+        //clear array or leave put
+    }
+})()

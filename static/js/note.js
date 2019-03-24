abb58b960dc5aaf13f965351626a24e25476f29c
Nik B <nikolaus.bertino@gmail.com>
2019-03-23 23:21:34 -0400

    added: delete method

--- static/js/note.js
+++ static/js/note.js
@@ -13,28 +13,32 @@ const Note = function Note(){
 			const params = new URLSearchParams(window.location.search);
 			if (params.has('note')) {
 				document.getElementById('filename').value = params.get('note');
-				(async function getData(){
-					const getTextData = await (function getTextData(filename) {
-						return fetch('/action.php?cmd=getData&filename=' + filename,{
-							method:'GET',
-						});
-					})(document.getElementById('filename').value);
-					let getJsonData;
-					try{
-						getJsonData = await getTextData.json();
-					}catch(e){
-						throw "Response could not be JSON parsed.";
-					}
-					if(getJsonData.success || getJsonData.success === ""){
-						document.getElementsByClassName('textarea')[0].innerText = atob(getJsonData.success);
-					}else{
-						throw "Unable to get note.";
-					}
-				})();
+				Module.getData();
 			}
 		})
 	}
-	
+	Module.getData = (filename) => {
+		(async function getData(){
+			const getTextData = await (function getTextData(filename) {
+				return fetch('/action.php?cmd=getData&filename=' + filename,{
+					method:'GET',
+				});
+			})(filename !== undefined ? filename : document.getElementById('filename').value);
+			let getJsonData;
+			try{
+				getJsonData = await getTextData.json();
+				document.getElementById('filename').value = filename !== undefined ? filename : document.getElementById('filename').value;
+			}catch(e){
+				throw "Response could not be JSON parsed.";
+			}
+			if(getJsonData.success || getJsonData.success === ""){
+				document.getElementsByClassName('textarea')[0].innerText = atob(getJsonData.success);
+			}else{
+				throw "Unable to get note.";
+			}
+		})();
+
+	}
 	Module.getListItems = () =>{
 		const url = '/action.php?cmd=getListItems';
 		(async function gl(){
@@ -46,15 +50,54 @@ const Note = function Note(){
 			const result = await getList.json();
 			if(result.success){
 				document.querySelector('.list').innerHTML = `${JSON.parse(result.success).map((val)=>{
-					return `<a href="/?note=${val}"><li>${val}</li></a>`;
+					return `<a href="/#/${val}"><li class="getNote" data="${val}">${val}<i class="fa fa-trash-o fa-lg delete" data="${val}"></i></li></a>`;
 				}).join("")}`;
+				[...document.querySelectorAll('.getNote')].map((a)=>{
+					a.addEventListener('click',(e)=>{
+						Module.getData(e.target.getAttribute('data'));
+
+					})
+				});
+				[...document.querySelectorAll('.delete')].map((a)=>{
+					a.addEventListener('click',(e)=>{
+						e.preventDefault();
+						Module.delete(e.target.getAttribute('data'));
+						Module.getListItems();
+						
+					})
+				});
 			}else{
 				throw "Unable to get List Items.";
 			}
 		})();
 	}
-	Module.save = () => {
-		const url = '/action.php?cmd=save&filename='+document.getElementById('filename').value;
+	Module.delete = (filename) => {
+		const url = '/action.php?cmd=delete&filename='+ (filename !== undefined ? filename : document.getElementById('filename').value);
+		(async function del(){
+			const getData = await (function(){
+				return fetch(url,{
+					method:'DELETE',
+					headers: {'Content-type':'application/x-www-form-urlencoded'},
+				})
+			})();
+			let result;
+			try{
+				result = await getData.json();
+			}catch(e){
+				throw "Result could not be JSON parsed."; 
+			} 
+			if(result.success){
+				document.getElementById('filename').value = result.success;
+				if(result.warning){
+					console.log(result.warning);
+				}
+			}else if(!noQueryParams && !queryParams.has("note")){
+				throw "Unable to get file.";
+			}
+		})()
+	}
+	Module.save = (filename) => {
+		const url = '/action.php?cmd=save&filename='+ (filename !== undefined ? filename : document.getElementById('filename').value);
 		(async function sav(){
 			const getData = await (function(){
 				return fetch(url,{
@@ -72,7 +115,6 @@ const Note = function Note(){
 			} 
 			if(result.success){
 				document.getElementById('filename').value = result.success;
-				console.log('saved');
 				if(result.warning){
 					console.log(result.warning);
 				}

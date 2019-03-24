3fce833cbebb874becf36fc41d6a77459d3be83f
Nik B <nikolaus.bertino@gmail.com>
2019-03-24 13:19:23 -0400

    removing die stmnts; modifying test cases

--- action.php
+++ action.php
@@ -3,29 +3,34 @@ register_shutdown_function( "fatal_handler" );
 
 class Main{
     
-    private $noteDir = "./notes/", $noteExtension = ".txt";
+    private $noteDir = "./notes/", $noteExtension = ".txt", $allTestCases = false, $isCli = false,
+            $definedCases = array();
     
     public function __construct($arg=null){
         if($arg === 'cli'){
+            $this->isCli = true;
             $numberArgs = count($_SERVER['argv']);
             if($numberArgs > 1){
                 $args = array_splice($_SERVER['argv'], 1);
                 $this->testCases($args[0]);
             }  
         }
+        $response = $this->actions();
+        die( sprintf("%s\n", $response ) );
+    }
+    private function actions(){
         $action = $this->determineAction();
         if($action['isGetData']){
-            $this->getData();
+            return $this->getData();
         }else if($action['isSaveData']){
-            $this->saveData();
+            return $this->saveData();
         }else if($action['isGetListItems']){
-            $this->getListItems();
+            return $this->getListItems();
         }else if($action['isCreateNote']){ 
-            $this->createNote();
+            return $this->createNote();
         }else if($action['isDelete']){
-            $this->deleteNote();
+            return $this->deleteNote();
         }
-    
     }
     private function determineAction(){
 
@@ -50,7 +55,7 @@ class Main{
     public function getData(){
         $file = $this->noteDir.$_GET['filename'].$this->noteExtension;
         $data = file_get_contents($file);
-        die(json_encode(array('success' => $data)));
+        return (json_encode(array('success' => $data)));
     }
     public function saveData(){
         $warning = "";
@@ -63,7 +68,7 @@ class Main{
             $warning = "data saved may have been corrupted.";
             $fileCreated = file_put_contents($this->noteDir.$name.$this->noteExtension, $text);
         }
-        die(json_encode(array('success' => $fileCreated ? $name : $fileCreated, 'warning' => $warning)));
+        return (json_encode(array('success' => $fileCreated ? $name : $fileCreated, 'warning' => $warning)));
     }
     public function getListItems(){
         $dirs = scandir($this->noteDir);
@@ -74,22 +79,23 @@ class Main{
             return str_replace($this->noteExtension, "", $a);
         };
         $noExtension = array_map($removeExt, array_filter($dirs, $removeHidden));
-        die(json_encode(array('success' => json_encode( array_values($noExtension)))));
+        return (json_encode(array('success' => json_encode( array_values($noExtension)))));
     }
     public function createNote(){
         $text =  json_decode($_POST['data'],true)['text'];
         $time = time();
         $name = $this->noteDir.$time.$this->noteExtension; 
         $fileCreated = file_put_contents($name, $text);
-        die(json_encode(array('success' => $fileCreated ? $time : $fileCreated))); 
+        return (json_encode(array('success' => $fileCreated !== FALSE ? $time : $fileCreated))); 
     }
     public function deleteNote(){
         $name = $_GET['filename'];
         $fileDeleted = unlink($this->noteDir.$name.$this->noteExtension);
-        die(json_encode(array('success' => $fileDeleted )));
+        return (json_encode(array('success' => $fileDeleted )));
     }
     private function testCases($arg){
-        $definedCases = [
+        
+        $this->definedCases = [
             'noFileName',
             'isGetData',
             'isGetData_fail',
@@ -100,46 +106,58 @@ class Main{
             'isDelete_fail',
             'listTestCases'
         ];
-        switch($arg){
-            case 'noFileName':
-                $_GET['filename'] = '';
-                break;
-            case 'isGetData':
-                $_GET['filename'] = "testfilename";
-                $_GET['cmd'] = 'getData';
-                break;
-            case 'isGetData_fail':
-                $_GET['filename'] = "";
-                $_GET['cmd'] = 'getData';
-                break;
-            case 'isSaveData':
-                $_GET['cmd'] = 'save';
-                $_GET['filename'] = "filename";
-                break;
-            case 'isSaveData_fail':
-                $_GET['cmd'] = 'save';
-                $_GET['filename'] = "";
-                break;
-            case 'isCreateNote':
-                $_GET['filename'] = "";
-                break;
-            case 'isDelete':
-                $_GET['filename'] = 'filename';
-                $_GET['cmd'] = 'delete';
-                $_SERVER['REQUEST_METHOD'] = 'DELETE';
-                break;
-            case 'isDelete_fail':
-                $_GET['filename'] = '';
-                $_GET['cmd'] = 'delete';
-                $_SERVER['REQUEST_METHOD'] = 'DELETE';
-                break;
-            case 'listTestCases':
-                die(var_dump($definedCases));
-                break;
-            default:
-                echo sprintf("%s\n", $arg);
-                break;
+        $runTest = function($case){
+            switch($case){
+                case 'noFileName':
+                    $_GET['filename'] = '';
+                    break;
+                case 'isGetData':
+                    $_GET['filename'] = "testfilename";
+                    $_GET['cmd'] = 'getData';
+                    break;
+                case 'isGetData_fail':
+                    $_GET['filename'] = "";
+                    $_GET['cmd'] = 'getData';
+                    break;
+                case 'isSaveData':
+                    $_GET['cmd'] = 'save';
+                    $_GET['filename'] = "filename";
+                    $data = array('text'=> base64_encode('test'));
+                    $_POST['data'] = json_encode($data);
+                    break;
+                case 'isSaveData_fail':
+                    $_GET['cmd'] = 'save';
+                    $_GET['filename'] = "";
+                    break;
+                case 'isCreateNote':
+                    $_GET['filename'] = "";
+                    break;
+                case 'isDelete':
+                    $_GET['filename'] = 'filename';
+                    $_GET['cmd'] = 'delete';
+                    $_SERVER['REQUEST_METHOD'] = 'DELETE';
+                    break;
+                case 'isDelete_fail':
+                    $_GET['filename'] = '';
+                    $_GET['cmd'] = 'delete';
+                    $_SERVER['REQUEST_METHOD'] = 'DELETE';
+                    break;
+                case 'listTestCases':
+                    die(var_dump($this->definedCases));
+                    break;
+                default:
+                    echo sprintf("%s\n", $arg);
+                    break;
+            }
+            if($this->allTestCases) var_dump(get_defined_vars());
+        };
+        if($arg === 'allTestCases'){
+            $this->allTestCases = true;
+            array_map($runTest, $definedCases);
+        }else{
+            $runTest($arg);
         }
+        
     }
 }
 if(php_sapi_name() == "cli"){
